a0beaa48e833978cce70915ab0d55062bce5d2c6
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-10-19 17:23:39 +0200

    Global SCSS + footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,10 +2,24 @@ import React from 'react'
 
 import './Footer.scss'
 
+import movieDBlogo from '../assets/img/moviedb-logo.png'
+
 const Footer: React.FunctionComponent = () => {
   return (
     <footer className='footer'>
-      <p>footer</p>
+      <div className='footer-credits'>
+        <a
+          href='https://www.themoviedb.org/about/logos-attribution'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <img className='moviedb-logo' src={movieDBlogo} alt='powered by the MovieDB API' />
+        </a>
+      </div>
+
+      <div className='footer-note'>
+        <q>Television is chewing gum for the eyes.</q>
+      </div>
     </footer>
   )
 }
