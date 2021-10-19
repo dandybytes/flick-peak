fd8d1bad72857c7e0febe0709a16d2fa103bad1a
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-10-19 20:00:07 +0200

    Create the loading indicator

---
+++ client/src/components/common/LoadingIndicator.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './LoadingIndicator.scss'
+
+const LoadingIndicator = (): JSX.Element => (
+  <div className='spinner'>
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+)
+
+export default LoadingIndicator
