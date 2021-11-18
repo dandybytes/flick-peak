937c0104b185c0e8245cad46df1f3bccbf941a0b
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-11-18 23:38:30 +0100

    Add static asset directory

--- server/server.js
+++ server/server.js
@@ -8,15 +8,25 @@ dotenv.config()
 
 const app = express()
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
-}
-
 // enable accepting JSON data in the body
 app.use(express.json())
 
 app.get('/api', (req, res) => {
   res.send('API running')
 })
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+if (process.env.NODE_ENV === 'production') {
+  // Set the static asset directory
+  app.use(express.static('client/build'))
+
+  // Serve index.html on all routes
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.listen(port, console.log(`server running in ${process.env.NODE_ENV} mode on port ${port}`))
