8b5cf61d90803fc52443056917b76ee35be76ed2
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-11-18 23:54:22 +0100

    Add email regex and user model

---
+++ server/models/userModel.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+import {emailRegex} from '../utils/regex.js'
+
+const userSchema = mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: emailRegex
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const User = mongoose.model('User', userSchema)
+
+export default User
