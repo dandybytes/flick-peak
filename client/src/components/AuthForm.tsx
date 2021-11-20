4e92da6715b44f660d15c9bdab0556b36f2e754b
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-11-20 23:58:15 +0100

    Create auth form

---
+++ client/src/components/AuthForm.tsx
@@ -0,0 +1,74 @@
+import {FC, FormEvent, useState} from 'react'
+import {Link} from 'react-router-dom'
+
+import './AuthForm.scss'
+
+type AuthFormProps = {
+  isLoginForm: boolean
+  handleSubmit: (event: FormEvent) => Promise<void>
+}
+
+const AuthForm: FC<AuthFormProps> = ({handleSubmit, isLoginForm}) => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmedPassword, setConfirmedPassword] = useState('')
+
+  return (
+    <div className='form-container'>
+      <h1 className='form-headline'>{isLoginForm ? 'Log In' : 'Create Account'}</h1>
+      <form className='form-body' onSubmit={handleSubmit}>
+        {!isLoginForm && (
+          <input
+            name='name'
+            type='text'
+            placeholder='name'
+            className='form-input'
+            value={name}
+            onChange={event => setName(event.target.value)}
+          />
+        )}
+        <input
+          name='email'
+          type='email'
+          placeholder='email'
+          className='form-input'
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
+        <input
+          name='password'
+          type='password'
+          placeholder='password'
+          className='form-input'
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+        {!isLoginForm && (
+          <input
+            name='confirm'
+            type='password'
+            placeholder='confirm password'
+            className='form-input'
+            value={confirmedPassword}
+            onChange={event => setConfirmedPassword(event.target.value)}
+          />
+        )}
+        <button className='submit-button' type='submit'>
+          {isLoginForm ? 'Sign In' : 'Sign Up'}
+        </button>
+      </form>
+      {isLoginForm ? (
+        <p className='form-footer'>
+          Don't have an account yet? <Link to={'/signup'}>Sign Up</Link>
+        </p>
+      ) : (
+        <p className='form-footer'>
+          Already have an account? <Link to={'/login'}>Log In</Link>
+        </p>
+      )}
+    </div>
+  )
+}
+
+export default AuthForm
