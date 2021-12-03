f4426a11f0b1da1307f46418e38bbf98b7b54e8c
dandybytes <42968783+dandybytes@users.noreply.github.com>
2021-12-03 23:37:27 +0100

    Make film strip glow on hover

--- client/src/components/FilmStrip.tsx
+++ client/src/components/FilmStrip.tsx
@@ -2,33 +2,33 @@ import cinemaIcon from '../assets/img/cinema.png'
 
 import './FilmStrip.scss'
 
-const FilmStrip = () => {
-  return (
-    <div className='filmstrip'>
-      <div className='filmstrip-container'>
-        <div className='filmstrip-perforations' />
-        <div className='filmstrip-content'>
-          <div className='filmstrip-frame'>
-            <img src={cinemaIcon} alt='cinema logo' />
-          </div>
-          <div className='filmstrip-frame'></div>
-          <div className='filmstrip-frame'>Flick</div>
-          <div className='filmstrip-frame'></div>
-          <div className='filmstrip-frame'>Pick</div>
-          <div className='filmstrip-frame'></div>
-          <div className='filmstrip-frame'>
-            <img src={cinemaIcon} alt='cinema logo' />
-          </div>
-          <div className='filmstrip-frame'></div>
-          <div className='filmstrip-frame'>Flick</div>
-          <div className='filmstrip-frame'></div>
-          <div className='filmstrip-frame'>Pick</div>
-          <div className='filmstrip-frame'></div>
+const FilmStrip = ({willGlowOnHover = true}: {willGlowOnHover?: boolean}) => (
+  <div className={`filmstrip ${willGlowOnHover ? 'hover-glow' : ''}`}>
+    <div className='filmstrip-container'>
+      <div className='filmstrip-perforations' />
+
+      <div className='filmstrip-content'>
+        <div className='filmstrip-frame'>
+          <img src={cinemaIcon} alt='cinema logo' />
         </div>
-        <div className='filmstrip-perforations' />
+        <div className='filmstrip-frame'></div>
+        <div className='filmstrip-frame'>Flick</div>
+        <div className='filmstrip-frame'></div>
+        <div className='filmstrip-frame'>Pick</div>
+        <div className='filmstrip-frame'></div>
+        <div className='filmstrip-frame'>
+          <img src={cinemaIcon} alt='cinema logo' />
+        </div>
+        <div className='filmstrip-frame'></div>
+        <div className='filmstrip-frame'>Flick</div>
+        <div className='filmstrip-frame'></div>
+        <div className='filmstrip-frame'>Pick</div>
+        <div className='filmstrip-frame'></div>
       </div>
+
+      <div className='filmstrip-perforations' />
     </div>
-  )
-}
+  </div>
+)
 
 export default FilmStrip
